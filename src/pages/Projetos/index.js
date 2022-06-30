badfe172a50469957f16191de99e61bd4cc68e12
larissacard <larissak.cardosom@gmail.com>
2022-06-30 10:19:01 -0300

    up

--- src/pages/Projetos/index.js
+++ src/pages/Projetos/index.js
@@ -15,55 +15,54 @@ function Projetos() {
                     <div>
                         <img src="assets/logo.svg"></img>
                     </div>
-                    <a href="/projetos">
-                        <div className="btn1">
-                            <div className="btn1_p1">
-                                <img width="30px" height="30px" src="assets/projetos_active.svg"></img>
-                                <h5>Projetos</h5>
+                    
+                    <div className="btmenu">
+                        <a href="/projetos">
+                            <div className="btn1">
+                                <div className="btn1_p1">
+                                    <img width="30px" height="30px" src="assets/projetos_active.svg"></img>
+                                    <h5>Projetos</h5>
+                                </div>
+                                <div className="btn1_p2"></div>
                             </div>
-                            <div className="btn1_p2"></div>
-                        </div>
-                    </a>
-
-                    <a href="/equipes">
-                        <div className="btn2">
-                            <div className="btn2_p1">
-                                <img width="30px" height="30px" src="assets/equipes.svg"></img>
-                                <h5>Equipes</h5>
+                        </a>
+                        <a href="/equipes">
+                            <div className="btn2">
+                                <div className="btn2_p1">
+                                    <img width="30px" height="30px" src="assets/equipes.svg"></img>
+                                    <h5>Equipes</h5>
+                                </div>
+                                <div className="btn2_p2"></div>
                             </div>
-                            <div className="btn2_p2"></div>
-                        </div>
-                    </a>
-
-                    <a href="/pessoas">
-                        <div className="btn2">
-                            <div className="btn2_p1">
-                                <img width="30px" height="30px" src="assets/pessoas.svg"></img>
-                                <h5>Pessoas</h5>
+                        </a>
+                        <a href="/pessoas">
+                            <div className="btn2">
+                                <div className="btn2_p1">
+                                    <img width="30px" height="30px" src="assets/pessoas.svg"></img>
+                                    <h5>Pessoas</h5>
+                                </div>
+                                <div className="btn2_p2"></div>
                             </div>
-                            <div className="btn2_p2"></div>
-                        </div>
-                    </a>
-
-                    <a href="/tarefas">
-                        <div className="btn2">
-                            <div className="btn2_p1">
-                                <img width="30px" height="30px" src="assets/tarefas.svg"></img>
-                                <h5>Tarefas</h5>
+                        </a>
+                        <a href="/tarefas">
+                            <div className="btn2">
+                                <div className="btn2_p1">
+                                    <img width="30px" height="30px" src="assets/tarefas.svg"></img>
+                                    <h5>Tarefas</h5>
+                                </div>
+                                <div className="btn2_p2"></div>
                             </div>
-                            <div className="btn2_p2"></div>
-                        </div>
-                    </a>
-
-                    <a href="/configuracoes">
-                        <div className="btn2">
-                            <div className="btn2_p1">
-                                <img width="30px" height="30px" src="assets/settings.svg"></img>
-                                <h5>Configurações</h5>
+                        </a>
+                        <a href="/configuracoes">
+                            <div className="btn2">
+                                <div className="btn2_p1">
+                                    <img width="30px" height="30px" src="assets/settings.svg"></img>
+                                    <h5>Configurações</h5>
+                                </div>
+                                <div className="btn2_p2"></div>
                             </div>
-                            <div className="btn2_p2"></div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 </Header>  
 
                 <div className="charts">
