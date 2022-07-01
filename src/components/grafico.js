841e376649b7d5f6a8719edeca964fd3b14b96c6
larissacard <larissak.cardosom@gmail.com>
2022-07-01 11:48:34 -0300

    up - pagina tarefas

--- src/components/grafico.js
+++ src/components/grafico.js
@@ -32,8 +32,8 @@ export function Grafico() {
   return (
     <Chart
       chartType="LineChart"
-      width="700px"
-      height="400px"
+      width="600px"
+      height="300px"
       data={data}
       options={options}
     />
--- src/pages/Pessoas/index.js
+++ src/pages/Pessoas/index.js
@@ -8,7 +8,7 @@ function Pessoas(){
         <div className="d-flex justify-content-center align-items-center" style={{width: "100vw", height: "100vh"}}>
 
             <div className="container_maior">
-            <Header>
+                <Header>
                     <div className="mt-4">
                         <img src="assets/logo.svg"></img>
                     </div>
@@ -62,8 +62,44 @@ function Pessoas(){
                     </div>
                 </Header>  
 
+                <div className="coluna-dois">
+                    <div className="cardp d-flex ">
+                        <div className="infopessoas"><h2>Bianca Stormi</h2>
+                        <p>Engenheira de Software</p></div>
+                        <div>
+                            <img src="assets/profile.svg"></img>
+                        </div>
+                    </div>
 
-                <Menu1 />
+                <div className="progresso-p">
+                    <div className="nome-icon d-flex">
+                        <h6>Status da Equipe</h6>
+                        <div className="d-flex pessoas-equipe">
+                            <img src="assets/people.svg"/>
+                            <img src="assets/people.svg"/>
+                            <img src="assets/people.svg"/>
+                            <img src="assets/people.svg"/>
+                        </div>
+                    </div>
+                    <p>Progresso</p>
+                    <div class="progress" style={{height: "8px", borderRadius: "50px"}}>
+                        <div class="progress-bar barra"  role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{borderRadius: "50px"}}></div>
+                    </div>
+                </div>
+                <div className="opcoes-pessoas d-flex justify-content-between">
+                    <div className="d-flex op">
+                        <h6>Concluido</h6>
+                        <h6 className="grey">Em desenvolvimento</h6>
+                    </div>
+                
+                    <div className="bttnsem">
+                        <button className="opbttn">Semanal
+                            <img src="assets/seta.svg"/>
+                        </button>
+                    </div> 
+                </div>     
+            </div>
+                
             </div>
         </div>
     );
