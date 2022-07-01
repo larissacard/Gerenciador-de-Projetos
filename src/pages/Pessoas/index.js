914089cddaf2248501a7075093f12fc3756bf2ed
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-07-01 15:41:22 -0300

    up

--- src/pages/Pessoas/index.js
+++ src/pages/Pessoas/index.js
@@ -64,42 +64,103 @@ function Pessoas(){
 
                 <div className="coluna-dois">
                     <div className="cardp d-flex ">
-                        <div className="infopessoas"><h2>Bianca Stormi</h2>
-                        <p>Engenheira de Software</p></div>
+                        <div className="infopessoas">
+                            <h2>Bianca Stormi</h2>
+                            <p>Engenheira de Software</p>
+                        </div>
                         <div>
                             <img src="assets/profile.svg"></img>
                         </div>
                     </div>
 
-                <div className="progresso-p">
-                    <div className="nome-icon d-flex">
-                        <h6>Status da Equipe</h6>
-                        <div className="d-flex pessoas-equipe">
-                            <img src="assets/people.svg"/>
-                            <img src="assets/people.svg"/>
-                            <img src="assets/people.svg"/>
-                            <img src="assets/people.svg"/>
+                    <div className="progresso-p">
+                        <div className="nome-icon d-flex">
+                            <h6>Status da Equipe</h6>
+                            <div className="d-flex pessoas-equipe">
+                                <img src="assets/people.svg"/>
+                                <img src="assets/people.svg"/>
+                                <img src="assets/people.svg"/>
+                                <img src="assets/people.svg"/>
+                            </div>
+                        </div>
+                        <p>Progresso</p>
+                        <div class="progress" style={{height: "8px", borderRadius: "50px"}}>
+                            <div class="progress-bar barra"  role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{borderRadius: "50px"}}></div>
                         </div>
                     </div>
-                    <p>Progresso</p>
-                    <div class="progress" style={{height: "8px", borderRadius: "50px"}}>
-                        <div class="progress-bar barra"  role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{borderRadius: "50px"}}></div>
+                    <div className="opcoes-pessoas d-flex justify-content-between">
+                        <div className="d-flex op">
+                            <h6>Concluido</h6>
+                            <h6 className="grey">Em desenvolvimento</h6>
+                        </div>
+                    
+                        <div className="bttnsem">
+                            <button className="opbttn">Semanal
+                                <img src="assets/seta.svg"/>
+                            </button>
+                        </div> 
+                    </div>     
+                </div>
+
+                <div className="coluna_pessoas">
+                    <div className='bt'>
+                        <button  className='opbttn'>Filtros
+                            <img src="assets/seta.svg"></img>
+                        </button>
+                        <input id="placeholder" type="search" placeholder="Pesquise aqui..." className='search'></input>
+                        <a className='lupa'><img src="assets/search.svg"></img></a>
+                    </div>
+                    <div className="all_cards">
+                        <div className="card_pessoas_active d-flex">
+                            <div className="info_pessoas">
+                                <h2>Bianca Stormi</h2>
+                                <p>Engenheira de Software</p>
+                            </div>
+                            <div>
+                                <img className="profile" src="assets/profile.svg"></img>
+                            </div>
+                        </div>
+                        <div className="card_pessoas d-flex">
+                            <div className="info_pessoas">
+                                <h2>Bianca Stormi</h2>
+                                <p>Engenheira de Software</p>
+                            </div>
+                            <div>
+                                <img className="profile" src="assets/profile.svg"></img>
+                            </div>
+                        </div>
+                        <div className="card_pessoas d-flex">
+                            <div className="info_pessoas">
+                                <h2>Bianca Stormi</h2>
+                                <p>Engenheira de Software</p>
+                            </div>
+                            <div>
+                                <img className="profile" src="assets/profile.svg"></img>
+                            </div>
+                        </div>
+                        <div className="card_pessoas d-flex">
+                            <div className="info_pessoas">
+                                <h2>Bianca Stormi</h2>
+                                <p>Engenheira de Software</p>
+                            </div>
+                            <div>
+                                <img className="profile" src="assets/profile.svg"></img>
+                            </div>
+                        </div>
+                        <div className="card_pessoas d-flex">
+                            <div className="info_pessoas">
+                                <h2>Bianca Stormi</h2>
+                                <p>Engenheira de Software</p>
+                            </div>
+                            <div>
+                                <img className="profile" src="assets/profile.svg"></img>
+                            </div>
+                        </div>
+                        <div id="scroll">
+                            <img src="assets/scroll.png"/>
+                        </div>
                     </div>
                 </div>
-                <div className="opcoes-pessoas d-flex justify-content-between">
-                    <div className="d-flex op">
-                        <h6>Concluido</h6>
-                        <h6 className="grey">Em desenvolvimento</h6>
-                    </div>
-                
-                    <div className="bttnsem">
-                        <button className="opbttn">Semanal
-                            <img src="assets/seta.svg"/>
-                        </button>
-                    </div> 
-                </div>     
-            </div>
-                
             </div>
         </div>
     );
