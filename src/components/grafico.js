0ae102847641a9bd5d2f081ab00cc53eaa43ec46
larissacard <larissak.cardosom@gmail.com>
2022-07-05 10:53:21 -0300

    calendario

--- src/components/grafico.js
+++ src/components/grafico.js
@@ -1,25 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Chart } from "react-google-charts";
-import { nodeName } from "rsuite/esm/DOMHelper";
-import { scrollTopAnimation } from "rsuite/esm/utils";
 
 // dados
 export const data = [
-  ["Dia", "Essa semana", "Semana"],
-  [1, 0, 2],
-  [2, 2, 3],
-  [3, 3.9, 4],
-  [4, 1.7, 1.8],
-  [5, 1.9, 1.7],
-  [6, 8.8, 1.3],
-  [7, 7.6, 1.2],
-  [8, 1.3, 2.9],
-  [9, 6.9, 4.2],
-  [10, 2.8, 3.9],
-  [11, 5.3, 7.9],
-  [12, 6.6, 8.4],
-  [13, 4.8, 6.3],
-  [14, 4.2, 6.2],
+  ["Dia", "Tarefas Concluidas", "Não Concluidas"],
+  ["Seg", 0, 2],
+  ["Ter", 2, 3],
+  ["Qua", 3.9, 4],
+  ["Qui", 1.7, 1.8],
+  ["Sex", 1.9, 1.7],
+  ["Sab", 8.8, 1.3],
+  ["Dom", 7.6, 1.2],
 ];
 
 export const options = {
--- src/pages/Projetos/date.js
+++ src/pages/Projetos/date.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';  
-import 'react-calendar/dist/Calendar.css';
+import './date.css';
 
 function MyApp() {
   const [value, onChange] = useState(new Date());
