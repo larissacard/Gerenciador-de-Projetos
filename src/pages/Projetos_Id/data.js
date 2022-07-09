8c8f1a80cca657205ddd2f77441dbc5444202456
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-07-09 11:48:59 -0300

    Consumo da API para projetos/id

--- src/pages/Projetos_Id/data.js
+++ src/pages/Projetos_Id/data.js
@@ -1,45 +1,41 @@
-import React, { Component } from "react";
-import { isEqual } from "rsuite/esm/utils/dateUtils";
+import React, { useState, useEffect } from "react";
 import api from "../../api";
 
 const projetoPath = window.location.pathname;
 console.log(projetoPath);
 
-class ExibirDetalhesProjeto extends Component{
-    state = {
-        projetos: [],
-    }
+function ExibirDetalhesProjeto(){
+    const [projeto, setProjeto] = useState({});
 
     useEffect(() => {
-        const fetchProjetos = async () => {
-            try {
-                const response = await api.get(projetoPath);
-                const data = await response.json();
-                this.state({projetos: data.data})
-
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchProjetos();
+        api
+            .get(projetoPath)
+            .then((response => setProjeto(response.data)))
+            .catch((error) => {
+                console.log("Eita bixo, um erro ó: " + error)
+            })
     }, []);
 
+    const dados = projeto.dados
+    const equipes = projeto.equipes
+    console.log(equipes)
+    return(
+        <>
+            <h1>Projeto: </h1>
+            <ul>
+                <li>{dados?.pr_id}</li>
+                <li>{dados?.pr_nome}</li>
+                <li>{dados?.pr_descricao}</li>
+                <li>{dados?.pr_status}</li>
+                <li>{dados?.pr_data_criacao}</li>
+                <li>{dados?.pr_data_finalizacao}</li>
+            </ul>
 
-    render(){
-        const { projetos } = this.state;
-        console.log(projetos)
-        
-
-        return(
-            <>
-             {this.state.projetos.map((projetos) =>
-                    <div>{projetos.pr_nome}</div>
-                )};
-            </>
-        )
-        console.log(projetos)
-    }
-    
+            <h2>Equipes</h2>
+            <ul>
+            </ul>
+        </>
+    )    
 }
 
 export default ExibirDetalhesProjeto;
