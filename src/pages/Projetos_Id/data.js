310850d03f0810dd8e664961282bdf5c310f2c28
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-07-09 13:03:40 -0300

    Finalização do consumo da API em projetos/id

--- src/pages/Projetos_Id/data.js
+++ src/pages/Projetos_Id/data.js
@@ -1,41 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import api from "../../api";
 
 const projetoPath = window.location.pathname;
-console.log(projetoPath);
 
-function ExibirDetalhesProjeto(){
-    const [projeto, setProjeto] = useState({});
+class ExibirDetalhesProjeto extends Component {
+  state = {
+    dados: {},
+    equipes: [],
+    tarefas: [],
+  };
 
-    useEffect(() => {
-        api
-            .get(projetoPath)
-            .then((response => setProjeto(response.data)))
-            .catch((error) => {
-                console.log("Eita bixo, um erro ó: " + error)
-            })
-    }, []);
+  componentDidMount() {
+    fetch("https://api-brisa-nodejs-postgresql.herokuapp.com" + projetoPath)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({ dados: res.dados });
+        this.setState({ equipes: res.equipes });
+        this.setState({ tarefas: res.tarefas });
+      });
+  }
 
-    const dados = projeto.dados
-    const equipes = projeto.equipes
-    console.log(equipes)
-    return(
-        <>
-            <h1>Projeto: </h1>
+  render() {
+    const { dados } = this.state;
+    const { equipes } = this.state;
+    const { tarefas } = this.state;
+
+    return (
+      <>
+        <h1>Projeto:</h1>
+        <ul>
+          <li>Id: {dados?.pr_id}</li>
+          <li>{dados?.pr_nome}</li>
+          <li>{dados?.pr_descricao}</li>
+          <li>Status: {dados?.pr_status}</li>
+          <li>Data de Criação: {dados?.pr_data_criacao}</li>
+          <li>Data de Finalização: {dados?.pr_data_finalizacao}</li>
+        </ul>
+
+        <h2>Equipes</h2>
+        {equipes.map((e) => (
+          <ul key={e.eq_id}>
+            <li>
+              {e.eq_id} - {e.eq_nome}
+            </li>
+
             <ul>
-                <li>{dados?.pr_id}</li>
-                <li>{dados?.pr_nome}</li>
-                <li>{dados?.pr_descricao}</li>
-                <li>{dados?.pr_status}</li>
-                <li>{dados?.pr_data_criacao}</li>
-                <li>{dados?.pr_data_finalizacao}</li>
+              {e.pessoas.map((p, index) => (
+                <li key={index}>
+                  {p.pe_id} - {p.pe_nome}
+                </li>
+              ))}
             </ul>
+          </ul>
+        ))}
 
-            <h2>Equipes</h2>
-            <ul>
-            </ul>
-        </>
-    )    
+        <h2>Tarefas</h2>
+        {tarefas.map((t) => (
+          <ul key={t.tr_id}>
+            <li>
+              {t.tr_id} - {t.tr_nome}
+            </li>
+          </ul>
+        ))}
+      </>
+    );
+  }
 }
 
 export default ExibirDetalhesProjeto;
