2990b3d558a089236694bd21ebde6537864a2cc3
larissacard <larissak.cardosom@gmail.com>
2022-07-27 11:47:03 -0300

    up - button sorting

--- src/pages/Equipes/filter.js
+++ src/pages/Equipes/filter.js
@@ -1,4 +1,82 @@
- import React, {useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
+import api from "../../api";
+
+function App() {
+
+    const [equipes, setEquipes] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await api.get("/projetos")
+                setEquipes(response.data);
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        getData();
+    }, []);
+
+    const [sortState, setSortState] = useState("none");
+
+    const sortMethods = {
+        none: { method: (a, b) => null },
+        ascending: { method: undefined },
+        descending: { method: (a, b) => (a > b ? -1 : 1) },
+    };
+
+    var equipes2 = []
+
+
+   
+    function Concatenar(){
+        {equipes.sort(sortMethods[sortState].method).map(e => (
+            <li key={e.pr_id}>
+                {equipes2.push(e.pr_nome)}
+            </li>
+           
+        ))}
+        return(
+            equipes2.sort(sortMethods[sortState].method).map((f, index) => (
+                <li key={index}>{f}</li>
+            ))
+        )
+    }
+
+    return (
+        <div className="main">
+            <select defaultValue={'DEFAULT'} onChange={(e) => setSortState(e.target.value)}>
+                <option value="DEFAULT" disabled>None</option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+            </select>
+            <ul>
+                <Concatenar/>
+            </ul>
+        </div>
+    );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  import React, {useState, useEffect} from "react";
  import {Dropdown} from 'react-bootstrap';
 
  
@@ -29,14 +107,14 @@
      return <div>
          <div>
              <div>
-                {/* <Dropdown>
+                 <Dropdown>
                 <Dropdown.Toggle variant="" id="dropdown-basic">Filtros</Dropdown.Toggle>
                 <Dropdown.Menu>
                      <Dropdown.Item href="#" onClick={e => handleOrder('pr_id')}>Mais antigos</Dropdown.Item>
                      <Dropdown.Item href="#" onClick={e => handleOrder('pr_id')}>Mais recentes</Dropdown.Item>
                      <th>descricao</th>
                      </Dropdown.Menu>
-                </Dropdown> * */}
+                </Dropdown> 
                 <button className="btn" onClick={e => handleOrder('eq_id')}>Mais recentes/Mais Antigos</button>
                 <button className="btn">Ordem Alfabetica</button>
              </div>
@@ -72,4 +150,4 @@
      </div>
  }
 
- export default Show;
+ export default Show; */
