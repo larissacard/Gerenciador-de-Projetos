4af1ee3bb8325f70ed41774786d09ed3747c6eee
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-07-29 11:55:01 -0300

    Iniciando criação do pipefy

---
+++ src/pages/Projetos_Id/Pipefy/Board/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Container} from './styles'
+
+function Board() {
+  return (
+    <Container>
+      <List />
+      <List />
+      <List />
+      <List />
+    </Container>
+  )
+}
+
+export default Board;
---
+++ src/pages/Projetos_Id/Pipefy/List/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+// import { Container } from './styles';
+
+function List() {
+  return <div />;
+}
+
+export default List;
