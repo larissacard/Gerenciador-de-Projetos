9d7d05d269924b8b5d5aaa36fe0767638d4ba000
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-04 14:29:17 -0300

    Arrasta e solta em tarefas das pessoas

--- src/components/CardTarefasDaPessoa/index.js
+++ src/components/CardTarefasDaPessoa/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
 import { Container, Top, Prioridade, Bottom } from './styles';
+import { useDrag } from 'react-dnd'
 
 function CardTarefasDaPessoa(Props) {
   const status = ["Baixa", "Media", "Alta"]
+  const [{isDragging}, dragRef] = useDrag({
+    type: 'CARD',
+    item: {'id': Props.dados.tr_id, "status": Props.dados.tr_status},
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+
   return (
-    <Container>
+    <Container ref={dragRef} isDragging={isDragging}>
         <Top>
-          <h3>{Props.titulo}</h3>
-          <Prioridade style={{backgroundColor: Props.prioridade === 3 ? "#E74444" : Props.prioridade === 2 ? "#FF9533" : Props.prioridade === 1 ? "#67CB65" : "#666"}}>
-            {status[Props.prioridade - 1]}
+          <h3>{Props.dados.tr_nome}</h3>
+          <Prioridade style={{backgroundColor: Props.dados.tr_prioridade === 3 ? "#E74444" : Props.dados.tr_prioridade === 2 ? "#FF9533" : Props.dados.tr_prioridade === 1 ? "#67CB65" : "#666"}}>
+            {status[Props.dados.tr_prioridade - 1]}
           </Prioridade>
         </Top>
         <Bottom>
           <p>Data de Criação:</p>
-          <span>{Props.data.substring(0,12)}</span>
+          <span>{Props.dados.tr_data_criacao.substring(0,12)}</span>
         </Bottom>
     </Container>
   );
--- src/components/Lista/index.js
+++ src/components/Lista/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import { useDrop } from 'react-dnd'
 import { Container, List } from './styles';
+import api from '../../api';
 
 function Lista(Props) {
+  console.log(Props)
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item){
+      if (item.status !== Props.status) {
+        console.log(item)
+        console.log(Props.status)
+        item.status = Props.status
+
+        const updateStatus = async () => {
+          const response = await api.put(`/tarefas/${item.id}/status/${Props.status}`)
+          console.log(response.data)
+          Props.func()
+        }
+        updateStatus()
+      }
+    }
+  })
   return (
-    <Container style={{width: Props.width, height: Props.height}}>
+    <Container style={{width: Props.width, height: Props.height}} ref={dropRef}>
       <h3>{Props.titulo}</h3>
       <List>{Props.children}</List>
     </Container>  
