ac3c9886a11f265d859c89436d39e5d0bbd776e9
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-08-18 11:34:25 -0300

    Criação do Detalhe de cada Tarefa

--- src/components/KanbanLi/styles.js
+++ src/components/KanbanLi/styles.js
@@ -4,47 +4,41 @@ export const Container = styled.li`
     background: rgba(150, 178, 253, 0.3);
     border: 2px solid rgba(150, 178, 253, 0);
     border-radius: 20px;
-
     width: calc(100%-28px);
-    min-height: 133px;
+    min-height: 70px;
     cursor: grab;
     padding: 10px;
-
     overflow-x: hidden;
-
+    cursor: pointer;
+    
     ${props => props.isDragging && css`
-        border: 2px dashed rgba(0,0,0,.2);
-        box-shadow: none;
-        background: transparent;
-
-        * {
-            opacity: 0;
-        }
+    border: 2px dashed rgba(0,0,0,.2);
+    box-shadow: none;
+    background: transparent;
+    
+    * {
+        opacity: 0;
+    }
     `}
-`;
+    `;
+
 
 export const Top = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: flex-start;
     margin-bottom: 10px;
-
+    
     h3 {
         font-weight: 600;
-        font-size: 15px;
+        font-size: 16px;
         color: #280948;
-        width: calc(100% - 75px);
+        width:100%;
         line-height: 22px;
         text-overflow: ellipsis;
         overflow: hidden;
     }
-
-    span {
-        border-radius: 20px;
-        padding: 0px 10px;
-        color: #fff;
-    }
-`;
+    `;
 
 export const Body = styled.div`
     ul {
@@ -64,5 +58,15 @@ export const Body = styled.div`
         font-size: 12px;
         font-weight: 600;
         color: #280948;
+    }
+`;
+
+export const Prioridade = styled.div`
+    display: flex;
+    
+    span {
+        border-radius: 20px;
+        padding: 0px 10px;
+        color: #fff;
     }
 `;
