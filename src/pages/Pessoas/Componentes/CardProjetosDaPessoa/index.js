2312e5c616a3a37443812058864b1b256d42971f
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-09-12 09:17:20 -0300

    Ajustes na tela de pessoas

--- src/pages/Pessoas/Componentes/CardProjetosDaPessoa/index.js
+++ src/pages/Pessoas/Componentes/CardProjetosDaPessoa/index.js
@@ -4,6 +4,7 @@ import { Container} from './styles';
 
 function CardProjetosDaPessoa(Props) {
   return (
+    Props.titulo.lenght > 0 &&
     <Container>
       <h3>{Props.titulo[0].nome}</h3>
     </Container>
