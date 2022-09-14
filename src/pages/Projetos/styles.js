32db1f98aa149281ce1d14ee7dc498b57cc4fe87
larissacard <larissak.cardosom@gmail.com>
2022-09-14 09:38:28 -0300

    Revert "fix bugs"
    This reverts commit d1fed66a9ea0738ca5d465c4cc0c608c4f2cc025.

--- src/pages/Projetos/styles.js
+++ src/pages/Projetos/styles.js
@@ -10,14 +10,13 @@ export const ColunaUm = styled.div`
     border-radius: 20px;
     overflow: hidden;
 
-    background-color: white;
+    background-color: black;
 
     display: flex;
     flex-direction: column;
 
     @media (max-width: 480px) {
         width: 100%;
-
         overflow: visible;
 
         margin: 0;
@@ -48,19 +47,22 @@ export const ColunaDois = styled.div`
     height: calc(100% - 48px);
     margin: 24px 10px;
     padding: 15px 20px;
-    background-color: white;
+    background-color: red;
     border-radius: 20px;
     
     display: flex;
     flex-direction: column;
 
     @media (max-width: 1600px) {
-
+        overflow: hidden;
         max-height: 85vh; 
     }
 
     @media (max-width: 480px) {
         width: 100%;
+        
+        overflow: scroll;
+
         margin: 0;
 
         border-radius: 0;
