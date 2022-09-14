09248286c5bcf016379029483ed56af4fdc6f710
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-09-14 11:54:49 -0300

    responsividade da janela de erro 404

--- src/pages/NotFound/styles.js
+++ src/pages/NotFound/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import erro404 from './assets/erro404.svg'
+import mobileerro404 from './assets/erro404_mobile.svg'
 
 export const Container = styled.div`
     width: 100vw;
@@ -23,5 +24,14 @@ export const Image = styled.div`
     margin-bottom: 20px;
     background-image: url(${erro404});
     width: 712px;
-    height: 500px; 
+    height: 500px;
+    
+    @media (max-width: 480px) {
+        margin-top: 150px;
+        margin-bottom: 30px;
+        background-image: url(${mobileerro404});
+        width: 380px;
+        height: 366px;
+        word-wrap: normal;
+    }
 `;
